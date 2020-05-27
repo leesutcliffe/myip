30e95e5804422918ddcaa4b43aaa526c49ce73e3
Lee Sutcliffe <lee@netlynx.co.uk>
2020-05-27 09:38:56 +0100

    started unit testing for http.get

--- main_test.go
+++ main_test.go
@@ -1,6 +1,23 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"net/http/httptest"
+	"net/http"
+)
+
+func TestGetRequest(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte(`OK`))
+	}))
+	defer ts.Close()
+
+	want:= "OK"
+	got := getReq(ts.URL)
+	if got != want {
+		t.Errorf("getReq() = %q, want %q", got, want)
+	}
+}
 
 func TestDefaultUrl(t *testing.T) {
 	want := "https://api.ipify.org"
